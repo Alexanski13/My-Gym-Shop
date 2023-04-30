37e5948896123e49dce1b30c6db797edbe044fd3
Alexanski13 <alexanderivanov344@gmail.com>
2023-04-30 11:32:38 +0300

    fixed i18n setting

--- my-gym-shop/src/main/java/bg/softuni/mygymshop/config/WebConfig.java
+++ my-gym-shop/src/main/java/bg/softuni/mygymshop/config/WebConfig.java
@@ -7,17 +7,19 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        CookieLocaleResolver clr = new CookieLocaleResolver();
-        clr.setCookieDomain("lang");
-        return clr;
+        SessionLocaleResolver slr = new SessionLocaleResolver ();
+        slr.setDefaultLocale(new Locale("en"));
+        return slr;
     }
 
     @Bean
